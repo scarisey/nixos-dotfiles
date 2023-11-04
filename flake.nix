32fdbb20f0674f9d416116727fbc4fe046976345
Sylvain Carisey <sylvain@carisey.dev>
2023-11-04 10:58:06 +0100

    chore:update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/b21a8c71aa75e67bd58eb709f4506a76c7fd28a1";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/63678e9f3d3afecfeafa0acead6239cdb447574c";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
