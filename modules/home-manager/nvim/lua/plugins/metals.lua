6c18414dc330f99ab4309507ede7e61762befa13
Sylvain Carisey <sylvain@carisey.dev>
2023-11-17 14:48:12 +0100

    feat(nvim):metals again

--- modules/home-manager/nvim/lua/plugins/metals.lua
+++ modules/home-manager/nvim/lua/plugins/metals.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-vsnip" },
+      { "hrsh7th/vim-vsnip" },
+    },
+  },
+  {
     "scalameta/nvim-metals",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -17,21 +25,24 @@ return {
     keys = { { "<leader>mm", "<cmd>:lua require'telescope'.extensions.metals.commands()<cr>" } },
     init = function()
       local metals_config = require("metals").bare_config()
-      -- Example of settings
+      metals_config.init_options.statusBarProvider = "on"
       metals_config.settings = {
         showImplicitArguments = true,
         excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
       }
-
-      -- *READ THIS*
-      -- I *highly* recommend setting statusBarProvider to true, however if you do,
-      -- you *have* to have a setting to display this in your statusline or else
-      -- you'll not see any messages from metals. There is more info in the help
-      -- docs about this
-      -- metals_config.init_options.statusBarProvider = "on"
-
-      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
       metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      metals_config.on_attach = function(client, bufnr)
+        require("metals").setup_dap()
+      end
+
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
 
       -- Debug settings if you're using nvim-dap
       local dap = require("dap")
@@ -55,11 +66,6 @@ return {
           },
         },
       }
-
-      metals_config.on_attach = function(client, bufnr)
-        require("metals").setup_dap()
-      end
-      require("metals").initialize_or_attach(metals_config)
     end,
   },
 }
