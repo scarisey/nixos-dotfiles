b6bb9342d475112ad931a70541aec70537f794aa
Sylvain Carisey <sylvain@carisey.dev>
2024-02-07 10:33:55 +0100

    chore:update antora

--- pkgs/antora/default.nix
+++ pkgs/antora/default.nix
@@ -1,31 +1,30 @@
 { lib, buildNpmPackage, fetchFromGitLab, jq, buildEnv }:
 let
   pname = "antora";
-  version = "3.1.5";
+  version = "3.1.7";
   originalFiles = fetchFromGitLab {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-PCtYV5jPGFja26Dv4kXiaw8ITWR4xzVP/9hc45UWHeg=";
+    hash = "sha256-uGXXp6boS5yYsInSmkI9S0Tn85QGVp/5Fsh1u3G4oPk=";
   };
   modifiedLock = ./package-lock.json;
+  npmDepsHash = "sha256-Tntd68OkuQF4sKk0Mgn6fEMSOBNqzqoaHNuBSDJnO3Y=";
+  mermaid-extension-version = "~0.0.4";
 in
 
 buildNpmPackage rec {
-  inherit pname version modifiedLock originalFiles;
+  inherit pname version modifiedLock originalFiles npmDepsHash;
   src = originalFiles;
 
-  npmDepsHash = "sha256-1C26WWa1UXVcWVnDewULV40GjRTekWEp9daJauCZoK0=";
-
-  # This is to stop tests from being ran, as some of them fail due to trying to query remote repositories
   postPatch = ''
-    cp ${modifiedLock} package-lock.json
-    substituteInPlace package.json --replace \
-      '"_mocha"' '""'
-    tmpfile=$(mktemp)
-    echo $tmpfile
-    ${jq}/bin/jq '.dependencies."@sntke/antora-mermaid-extension" = "~0.0.4"' ./package.json > $tmpfile
-    cp $tmpfile package.json
+      cp ${modifiedLock} package-lock.json
+    # This is to stop tests from being ran, as some of them fail due to trying to query remote repositories
+      substituteInPlace package.json --replace \
+        '"_mocha"' '""'
+      tmpfile=$(mktemp)
+      ${jq}/bin/jq '.dependencies."@sntke/antora-mermaid-extension" = "${mermaid-extension-version}"' ./package.json > $tmpfile
+      cp $tmpfile package.json
   '';
 
   postInstall = ''
@@ -37,6 +36,6 @@ buildNpmPackage rec {
     description = "A modular documentation site generator. Designed for users of Asciidoctor.";
     homepage = "https://antora.org";
     license = licenses.mpl20;
-    maintainers = [ maintainers.ehllie ];
+    #    maintainers = [ maintainers.ehllie ]; # copied from Nixpkgs
   };
 }
