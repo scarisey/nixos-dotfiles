ad4a1023874aec9a2b9ef3bc2a5b25d7a49f4e5e
Sylvain Carisey <sylvain@carisey.dev>
2024-03-17 22:41:19 +0100

    feat(testdebits)

--- pkgs/testdebits/default.nix
+++ pkgs/testdebits/default.nix
@@ -1,40 +1,42 @@
-{
-  stdenv,
-  lib,
-  bash,
-  makeWrapper,
-  coreutils,
-  curl,
-  ...
-}: let
+{ stdenv
+, lib
+, bash
+, makeWrapper
+, coreutils
+, curl
+, mtr
+, ...
+}:
+let
   fs = lib.fileset;
   sourceFiles = fs.difference ./. (fs.maybeMissing ./result);
 in
-  stdenv.mkDerivation {
-    pname = "testdebits";
-    version = "1.0";
+stdenv.mkDerivation {
+  pname = "testdebits";
+  version = "1.0";
 
-    src = fs.toSource {
-      root = ./.;
-      fileset = sourceFiles;
-    };
+  src = fs.toSource {
+    root = ./.;
+    fileset = sourceFiles;
+  };
 
-    nativeBuildInputs = [makeWrapper];
-    buildInputs = [bash coreutils curl];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ bash coreutils curl mtr ];
 
-    postInstall = ''
-      wrapProgram $out/bin/testdebits \
-        --set PATH ${lib.makeBinPath [
-        coreutils
-        curl
-      ]}
-    '';
+  postInstall = ''
+    wrapProgram $out/bin/testdebits \
+      --set PATH ${lib.makeBinPath [
+      coreutils
+      curl
+      mtr
+    ]}
+  '';
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/bin
-      cp testdebits $out/bin/testdebits
-      chmod a+x $out/bin/testdebits
-      runHook postInstall
-    '';
-  }
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp testdebits $out/bin/testdebits
+    chmod a+x $out/bin/testdebits
+    runHook postInstall
+  '';
+}
