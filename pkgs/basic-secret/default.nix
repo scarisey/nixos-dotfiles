88ebc5733b6f6cf8cc4be146077d4642fde9ebf1
Sylvain Carisey <sylvain@carisey.dev>
2024-03-25 21:06:55 +0100

    fix:basic-secret

--- pkgs/basic-secret/default.nix
+++ pkgs/basic-secret/default.nix
@@ -1,9 +1,12 @@
-{pkgs, ...}: let
-  pname = "basic-secret";
-  version = "0.1";
-in
-  pkgs.stdenv.mkDerivation {
-    inherit pname version;
+{ pkgs
+, stdenv
+, buildFHSEnv
+, ...
+}:
+let
+  basic-secret = stdenv.mkDerivation {
+    pname = "basic-secret";
+    version = "0.1";
 
     src = pkgs.fetchurl {
       url = "https://github.com/scarisey/basic-secret/releases/download/0.1/secret";
@@ -14,8 +17,16 @@ in
     dontUnpack = true;
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/bin
-      cp $src $out/bin/secret
-      chmod +x $out/bin/secret
+      cp $src $out/bin/basic-secret
+      chmod +x $out/bin/basic-secret
+      runHook postInstall
     '';
-  }
+  };
+in
+buildFHSEnv {
+  name = "secret";
+  targetPkgs = pkgs: [ basic-secret ];
+  runScript = "basic-secret";
+}
