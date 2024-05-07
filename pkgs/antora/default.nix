213027bd9adedf1b146a521596a6d64833fbf7a6
Sylvain Carisey <sylvain@carisey.dev>
2024-05-07 22:28:59 +0200

    fix(antora):install aux deps

--- pkgs/antora/default.nix
+++ pkgs/antora/default.nix
@@ -1,37 +1,41 @@
-{
-  lib,
-  buildNpmPackage,
-  fetchurl,
-  jq,
-  buildEnv,
-}: let
+{ lib
+, buildNpmPackage
+, fetchurl
+, jq
+, buildEnv
+,
+}:
+let
   source = lib.importJSON ./source.json;
 in
-  buildNpmPackage rec {
-    pname = "antora";
-    inherit (source) version;
-    src = fetchurl {
-      url = "https://registry.npmjs.org/antora/-/${source.filename}";
-      hash = source.integrity;
-    };
+buildNpmPackage rec {
+  pname = "antora";
+  inherit (source) version;
+  src = fetchurl {
+    url = "https://registry.npmjs.org/antora/-/${source.filename}";
+    hash = source.integrity;
+  };
 
-    postPatch = ''
-      ln -s ${./package-lock.json} package-lock.json
-    '';
+  postPatch = ''
+    rm package.json
+    ln -s ${./package.json} package.json
+    ln -s ${./package-lock.json} package-lock.json
+  '';
 
-    npmDepsHash = source.deps;
+  npmDepsHash = source.deps;
 
-    makeCacheWritable = true;
-    dontNpmBuild = true;
-    npmFlags = ["--omit=optional"];
+  makeCacheWritable = true;
+  dontNpmBuild = true;
+  dontNpmInstall = false;
+  npmFlags = [ "--omit=optional" ];
 
-    passthru = {
-      updateScript = ./update.sh;
-    };
+  passthru = {
+    updateScript = ./update.sh;
+  };
 
-    meta = with lib; {
-      description = "A modular documentation site generator. Designed for users of Asciidoctor.";
-      homepage = "https://antora.org";
-      license = licenses.mpl20;
-    };
-  }
+  meta = with lib; {
+    description = "A modular documentation site generator. Designed for users of Asciidoctor.";
+    homepage = "https://antora.org";
+    license = licenses.mpl20;
+  };
+}
