e35960591ea991f0be62b2b7548e3662b50bb07c
Sylvain Carisey <sylvain@carisey.dev>
2024-08-21 10:09:19 +0200

    doc:lib functions signatures

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }: rec {
   #lsDirs :: path -> [ dir1 dir 2 ... ]
   lsDirs = path: lib.mapAttrsToList (k: v: k) (lib.filterAttrs (k: v: v == "directory") (builtins.readDir path));
-  #users :: path -> { "user1@host1"=path1; "user2"@host1=path2; ...}
+  #users :: path -> [ {user="user1@host1";path=./user1;system="x86_64-linux";} ... ]
   #structure of path must be: path/user/host
   users = path: lib.flatten (
     map
@@ -24,7 +24,7 @@
       (lsDirs path)
   )
   ;
-  #forUsers :: path -> (user -> system -> userPath -> result) -> { "user1@host1"= result; }
+  #forUsers :: path -> ({user, system, path} -> result) -> { "user1@host1"= result; }
   forUsers = path: f: lib.mergeAttrsList (map ({ user, ... }@x: { "${user}" = (f x); }) (users path));
   #forHosts :: path -> (hostPath -> result) -> { "host1"= result; }
   forHosts = path: f: lib.genAttrs (lsDirs path) (x: f (path + "/${x}/configuration.nix"));
