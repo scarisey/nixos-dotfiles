0b1343420f1860d2a5ad8cf1e2c02953f34ffc4f
Sylvain Carisey <sylvain@carisey.dev>
2024-09-08 18:15:30 +0200

    feat(boot):quiet

--- modules/nixos/bootanimation.nix
+++ modules/nixos/bootanimation.nix
@@ -22,21 +22,21 @@ in {
       };
 
       # Enable "Silent Boot"
-      # consoleLogLevel = 0;
-      # initrd.verbose = false;
-      # kernelParams = [
-      #   "quiet"
-      #   "splash"
-      #   "boot.shell_on_fail"
-      #   "loglevel=3"
-      #   "rd.systemd.show_status=false"
-      #   "rd.udev.log_level=3"
-      #   "udev.log_priority=3"
-      # ];
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
       # # Hide the OS choice for bootloaders.
       # # It's still possible to open the bootloader list by pressing any key
       # # It will just not appear on screen unless a key is pressed
-      # loader.timeout = 0;
+      loader.timeout = 0;
     };
   };
 }
