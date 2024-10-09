15d91d69d4e438665af9b9f327459e5ea905099e
Sylvain Carisey <sylvain@carisey.dev>
2024-10-09 10:03:31 +0200

    fix:for ubuntu, stylix not applied

--- home-manager/sylvain/x86_64-linux/lscarisey/home.nix
+++ home-manager/sylvain/x86_64-linux/lscarisey/home.nix
@@ -28,8 +28,22 @@
     vscode
   ];
 
-  stylix.image = pkgs.fetchurl {
-    url = "https://www.pixelstalk.net/wp-content/uploads/image12/Minimalist-forest-lake-reflection-with-tall-trees-and-a-tranquil-glass-like-surface-set-in-a-peaceful-woodland.jpg";
-    hash = "sha256-6jSYP6zArfOx9psvGTXeljHlZ8EyDaHMJkb2OHGTYEo=";
+  stylix = {
+    autoEnable = false;
+    targets = {
+      bat.enable = true;
+      btop.enable = true;
+      firefox.enable = true;
+      fzf.enable = true;
+      k9s.enable = true;
+      kitty.enable = true;
+      neovim.enable = true;#will not work now
+      vim.enable = true;
+      yazi.enable = true;
+    };
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/image12/Minimalist-forest-lake-reflection-with-tall-trees-and-a-tranquil-glass-like-surface-set-in-a-peaceful-woodland.jpg";
+      hash = "sha256-6jSYP6zArfOx9psvGTXeljHlZ8EyDaHMJkb2OHGTYEo=";
+    };
   };
 }
