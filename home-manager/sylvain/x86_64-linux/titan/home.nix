b7cf5b47b3a16add1d34d95ebc6a459bceb23895
Sylvain Carisey <sylvain@carisey.dev>
2024-10-23 10:42:39 +0200

    chore:formatting

--- home-manager/sylvain/x86_64-linux/titan/home.nix
+++ home-manager/sylvain/x86_64-linux/titan/home.nix
@@ -20,6 +20,6 @@
   stylix.image = pkgs.fetchurl {
     url = "https://bitbucket.org/SCarSly/wallpapers/raw/c15754dfdbb687804f38e2e39d6bd86734de2269/pexels-eberhardgross-730981.jpg";
     hash = "sha256-HH+RVs8g+lF+E5XHXDXOR03vi1c8uivQ517uvbrQ7dA=";
-# nix-hash --type sha256 --to-sri $(nix-prefetch-url [URL])
+    # nix-hash --type sha256 --to-sri $(nix-prefetch-url [URL])
   };
 }
