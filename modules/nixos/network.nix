2a65f9593cf44a6933f77126f79c74dfb78e0788
Sylvain Carisey <sylvain@carisey.dev>
2024-11-15 22:33:31 +0100

    fix(network):no declarative network conf, bad idea

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -8,22 +8,11 @@ with lib; let
   cfg = config.scarisey.network;
 in {
   options.scarisey.network.enable = mkEnableOption "Network settings";
-  options.scarisey.network.systemd.enable = mkEnableOption "Use systemd network settings.";
 
   config = mkIf cfg.enable (mkMerge [
     {
-      networking.nameservers = ["8.8.8.8" "1.1.1.1"];
-      services.resolved = {
-        enable = true;
-        dnssec = "true";
-        domains = ["~."];
-        fallbackDns = [
-          "8.8.4.4"
-          "1.0.0.1"
-        ];
-        dnsovertls = "true";
-      };
-
+      networking.enableIPv6 = false;
+      networking.networkmanager.enable = true;
       environment.systemPackages = with pkgs; [
         dig
         netcat-openbsd
@@ -31,45 +20,5 @@ in {
       programs.mtr.enable = true; #improved traceroute+ping to detect packet loss
     }
 
-    (mkIf (!cfg.systemd.enable) {
-      networking.enableIPv6 = false;
-      networking.networkmanager.enable = true;
-      # These options are unnecessary when managing DNS ourselves
-      networking.useDHCP = false;
-      networking.dhcpcd.enable = false;
-    })
-
-    (mkIf cfg.systemd.enable {
-      systemd.network = {
-        enable = true;
-        wait-online.enable = false;
-
-        netdevs = {
-          "20-br0" = {
-            netdevConfig = {
-              Kind = "bridge";
-              Name = "br0";
-            };
-          };
-        };
-
-        networks = {
-          "30-enp39s0" = {
-            matchConfig.Name = "enp39s0";
-            networkConfig.Bridge = "br0";
-            linkConfig.RequiredForOnline = "enslaved";
-          };
-          "40-br0" = {
-            matchConfig.Name = "br0";
-            bridgeConfig = {};
-            networkConfig = {
-              Address = "192.168.1.229/24";
-              Gateway = "192.168.1.1";
-            };
-            linkConfig.RequiredForOnline = "no";
-          };
-        };
-      };
-    })
   ]);
 }
