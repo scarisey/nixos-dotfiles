6ed9ce00e1ab9dce219945f6661b1345c16ff3fa
Sylvain Carisey <sylvain@carisey.dev>
2024-12-07 12:33:23 +0100

    feat(hyperion):smartmontools

--- nixos/hyperion/configuration.nix
+++ nixos/hyperion/configuration.nix
@@ -82,6 +82,7 @@
   };
   environment.systemPackages = with pkgs; [
     qbittorrent
+    smartmontools
   ];
   networking.firewall = {
     allowedTCPPorts = [139 145 5357 8080 3389]; #SAMBA QBITTORENT RDP
