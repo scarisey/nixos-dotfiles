98684068f674a594a2771d6c1c2852a2440de456
Sylvain Carisey <sylvain@carisey.dev>
2024-12-15 17:01:13 +0100

    feat(lscarisey):new build

---
+++ nixos/lscarisey/configuration.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware.nix
+    ../common.nix
+  ];
+
+  networking.hostName = "lscarisey";
+  scarisey.network.enable = true;
+  scarisey.qemu.enable = true;
+  scarisey.gnome.enable = true;
+  virtualisation.waydroid.enable = true;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+    ];
+  };
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+  services.printing.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  services.gvfs.enable = true; #for samba
+  environment.systemPackages = with pkgs; [
+    samba
+    smartmontools
+  ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
