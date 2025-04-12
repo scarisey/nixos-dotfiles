8c1c73ed9684b9bc301995d3e2e160b0d7372b5a
Sylvain Carisey <sylvain@carisey.dev>
2025-04-12 17:48:42 +0200

    feat(prometheus):processes

--- nixos/hyperion/prometheus.nix
+++ nixos/hyperion/prometheus.nix
@@ -9,7 +9,10 @@
         job_name = "node";
         static_configs = [
           {
-            targets = ["localhost:${toString config.services.prometheus.exporters.node.port}"];
+            targets = [
+              "${config.services.prometheus.exporters.node.listenAddress}:${toString config.services.prometheus.exporters.node.port}"
+              "${config.services.prometheus.exporters.process.listenAddress}:${toString config.services.prometheus.exporters.process.port}"
+            ];
           }
         ];
       }
@@ -18,11 +21,22 @@
   services.prometheus.exporters.node = {
     enable = true;
     port = 9100;
+    listenAddress = "127.0.0.1";
     enabledCollectors = [
       "logind"
       "systemd"
       "processes"
     ];
-    openFirewall = true;
+  };
+  services.prometheus.exporters.process = {
+    enable = true;
+    port = 9101;
+    listenAddress = "127.0.0.1";
+    settings.process_names = [
+      {
+        name = "{{.Matches.Wrapped}} {{ .Matches.Args }}";
+        cmdline = ["^/nix/store[^ ]*/(?P<Wrapped>[^ /]*) (?P<Args>.*)"];
+      }
+    ];
   };
 }
