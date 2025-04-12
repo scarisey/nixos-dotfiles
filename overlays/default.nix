af9fb91e0458171e7b22debb418a844855d76001
Sylvain Carisey <sylvain@carisey.dev>
2025-04-13 00:24:00 +0200

    feat:new plex

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,15 @@
 {inputs, ...}: {
-  additions = final: prev: import ../pkgs {pkgs = final;} // {ghostty = inputs.ghostty.packages.${final.system}.default;};
+  additions = final: prev: let
+    unstable = import inputs.nixpkgs-master {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  in
+    import ../pkgs {pkgs = final;}
+    // {
+      ghostty = inputs.ghostty.packages.${final.system}.default;
+      plex = unstable.plex;
+    };
 
   modifications = final: prev: {
     jdk = final.jdk21;
