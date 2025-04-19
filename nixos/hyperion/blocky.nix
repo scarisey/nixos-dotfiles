fc7bc4d83796580f8cedc2126a7cf03018091e0b
Sylvain Carisey <sylvain@carisey.dev>
2025-04-19 16:32:12 +0200

    feat:blocky ipv6

--- nixos/hyperion/blocky.nix
+++ nixos/hyperion/blocky.nix
@@ -1,21 +1,26 @@
-{...}: {
+{...}: let
+  localv4 = "192.168.1.79";
+  localv6 = "fe80::291b:a25b:4e6d:5a84";
+in {
   services.blocky = {
     enable = true;
     settings = {
+      log.level="error";
       ports.dns = 53;
       ports.http = 4000;
       upstreams.groups.default = [
         "1.1.1.1"
         "8.8.8.8"
         "9.9.9.9"
+        "2606:4700:4700:0:0:0:0:1111"
+        "2001:4860:4860:0:0:0:0:8888"
       ];
       upstreams.strategy = "parallel_best";
       upstreams.timeout = "2s";
-      connectIPVersion = "v4";
       customDNS.mapping = {
-        "plex-hyperion.carisey.dev" = "192.168.1.79";
-        "grafana-hyperion.carisey.dev" = "192.168.1.79";
-        "zoneminder-hyperion.internal.carisey.dev" = "192.168.1.79";
+        "plex-hyperion.carisey.dev" = "${localv4},${localv6}";
+        "grafana-hyperion.carisey.dev" = "${localv4},${localv6}";
+        "zoneminder-hyperion.internal.carisey.dev" = "${localv4},${localv6}";
       };
 
       blocking = {
