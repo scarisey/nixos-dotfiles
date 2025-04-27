e57ded19daa06195553863e209074f3d44bcdc02
Sylvain Carisey <sylvain@carisey.dev>
2025-04-27 17:33:58 +0200

    refactor(gui):use wrapper when needed

--- modules/home-manager/gui.nix
+++ modules/home-manager/gui.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  inputs,
   ...
 }:
 with lib; let
@@ -10,34 +11,48 @@ in {
   options.scarisey.gui = {
     enable = mkEnableOption "Common GUI tools";
     obs = mkEnableOption "OBS Studio";
+    nixgl = {
+      enable = mkEnableOption "Enable NixGL wrappers";
+      defaultWrapper = mkOption {
+        type = lib.types.enum (builtins.attrNames config.lib.nixGL.wrappers);
+        default = "mesa";
+      };
+    };
   };
   config = mkIf cfg.enable (
     mkMerge [
       {
-        home.packages = with pkgs; [
-          opera
-          firefox
+        home.packages = with pkgs;
+          map (x: config.lib.nixGL.wrap x) [
+            opera
+            firefox
 
-          vlc
-          mpv
-          amberol
+            vlc
+            mpv
+            amberol
 
-          ghostty
-          neovide
+            ghostty
+            neovide
 
-          xclip
-          pavucontrol
-          flameshot
+            xclip
+            pavucontrol
+            flameshot
 
-          scrcpy
-
-          (mkIf cfg.obs obs-studio)
-        ];
+            scrcpy
+          ];
 
         home.file.".config/ghostty/config" = {
           source = ./ghostty/config;
         };
       }
+      (mkIf cfg.nixgl.enable {
+        nixGL.packages = inputs.nixgl.packages;
+        nixGL.defaultWrapper = cfg.nixgl.defaultWrapper;
+        nixGL.installScripts = [cfg.nixgl.defaultWrapper];
+      })
+      (mkIf cfg.obs {
+        home.packages = [(config.lib.nixGL.wrap pkgs.obs-studio)];
+      })
     ]
   );
 }
