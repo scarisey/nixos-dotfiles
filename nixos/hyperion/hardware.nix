f851499999386307b739b1ed79d553dddc8f782a
Sylvain Carisey <sylvain@carisey.dev>
2025-08-14 10:50:58 +0200

    feat(hyperion):power management for low power consumption

--- nixos/hyperion/hardware.nix
+++ nixos/hyperion/hardware.nix
@@ -7,6 +7,7 @@
 }: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   boot.initrd.availableKernelModules = ["nvme" "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["wireguard"];
@@ -35,6 +36,48 @@
 
   networking.useDHCP = lib.mkDefault true;
 
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  #AMD specific config for power management to work (AMD P-State)
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.cpu.amd.updateMicrocode = true;
+  boot.kernelParams = [
+    "quiet"
+    "amd_pstate=passive" # permet au scheduler de descendre la fréquence plus souvent
+    "acpi_enforce_resources=lax"
+  ];
+  powerManagement.cpuFreqGovernor = "powersave";
+  services.thermald.enable = true; # réduit les pics thermiques
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_AC = "low-power";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 40;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
+  };
+  hardware.enableAllFirmware = true;
+  environment.systemPackages = with pkgs; [
+    powertop
+    cpufetch
+    lm_sensors
+  ];
+  systemd.services.powertop-autotune = {
+    description = "Powertop auto-tune at boot";
+    after = [ "multi-user.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.powertop}/bin/powertop --auto-tune";
+    };
+  };
+
+
 }
