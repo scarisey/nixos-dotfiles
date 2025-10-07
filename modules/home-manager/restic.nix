b2a9ed00adb39c079df760c90ccf712f307001a8
Sylvain Carisey <sylvain@carisey.dev>
2025-10-07 15:48:38 +0200

    feat:restic home manager module

--- modules/home-manager/restic.nix
+++ modules/home-manager/restic.nix
@@ -6,6 +6,19 @@
 }:
 with lib; let
   cfg = config.scarisey.restic;
+  resticProton = pkgs.writeShellScriptBin "restic-proton" ''
+    export RESTIC_REPOSITORY="/home/sylvain/backupsRestic/"
+    export RESTIC_PASSWORD_FILE="${config.sops.secrets."restic/protonDrive".path}"
+    ${pkgs.restic}/bin/restic "$@"
+  '';
+  resticCronos = pkgs.writeShellScriptBin "restic-cronos" ''
+    export RESTIC_REPOSITORY="s3:https://s3.eu-central-003.backblazeb2.com/cronos-backups"
+    export RESTIC_PASSWORD_FILE="${config.sops.secrets."restic/cronos-backups/repositoryPwd".path}"
+    set -a
+    source ${config.sops.secrets."restic/cronos-backups/backblaze/envFile".path}
+    set +a
+    ${pkgs.restic}/bin/restic "$@"
+  '';
 in {
   options.scarisey.restic = {
     enable = mkEnableOption "Restic settings.";
@@ -13,8 +26,12 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       restic
+      resticCronos
+      resticProton
     ];
 
     sops.secrets."restic/protonDrive" = {};
+    sops.secrets."restic/cronos-backups/repositoryPwd" = {};
+    sops.secrets."restic/cronos-backups/backblaze/envFile" = {};
   };
 }
