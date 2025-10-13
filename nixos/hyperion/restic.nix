698f65355b874e0a59889fbd366398d3a17c3c2b
Sylvain Carisey <sylvain@carisey.dev>
2025-10-13 11:40:37 +0200

    feat(hyperion):audiobookshelf

--- nixos/hyperion/restic.nix
+++ nixos/hyperion/restic.nix
@@ -1,33 +1,37 @@
-{config,lib,pkgs,...}:
 {
-users.users.restic = { isNormalUser = true; };
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  users.users.restic = {isNormalUser = true;};
 
-security.wrappers.restic = {
-  source = "${pkgs.restic}/bin/restic";
-  owner = "restic";
-  group = config.scarisey.homelab.group;
-  permissions = "u=rwx,g=,o=";
-  capabilities = "cap_dac_read_search=+ep";
-};
-
-services.restic.backups.immich = {
-  repository="s3:https://s3.eu-central-003.backblazeb2.com/cronos-backups";
-  environmentFile = config.sops.secrets."restic/cronos-backups/backblaze/envFile".path;
-  passwordFile = config.sops.secrets."restic/cronos-backups/repositoryPwd".path;
-  initialize = true;
-  user = "restic";
-  paths = [
-    "${config.services.immich.mediaLocation}"
-  ];
-  timerConfig = {
-    OnCalendar = "daily";
-    Persistent = true;
+  security.wrappers.restic = {
+    source = "${pkgs.restic}/bin/restic";
+    owner = "restic";
+    group = config.scarisey.homelab.group;
+    permissions = "u=rwx,g=,o=";
+    capabilities = "cap_dac_read_search=+ep";
   };
-  pruneOpts = [
-    "--keep-last 3"
-  ];
-  package = pkgs.writeShellScriptBin "restic-wrapper" ''
-    exec /run/wrappers/bin/restic "$@"
-  '';
-};
+
+  services.restic.backups.immich = {
+    repository = "s3:https://s3.eu-central-003.backblazeb2.com/cronos-backups";
+    environmentFile = config.sops.secrets."restic/cronos-backups/backblaze/envFile".path;
+    passwordFile = config.sops.secrets."restic/cronos-backups/repositoryPwd".path;
+    initialize = true;
+    user = "restic";
+    paths = [
+      "${config.services.immich.mediaLocation}"
+    ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    pruneOpts = [
+      "--keep-last 3"
+    ];
+    package = pkgs.writeShellScriptBin "restic-wrapper" ''
+      exec /run/wrappers/bin/restic "$@"
+    '';
+  };
 }
