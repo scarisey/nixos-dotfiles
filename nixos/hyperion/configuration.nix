4030a9ad2f55927e97509c80f4f5a3764760d14d
Sylvain Carisey <sylvain@carisey.dev>
2025-11-28 11:34:53 +0100

    feat(hyperion):AMD GPU

--- nixos/hyperion/configuration.nix
+++ nixos/hyperion/configuration.nix
@@ -35,14 +35,9 @@
   };
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [vaapiIntel];
+    enable32Bit = true;
   };
+  hardware.amdgpu.opencl.enable = true;
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
