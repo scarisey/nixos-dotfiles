f4bfd1f203d291c128b3bc52e56c6031e80c9081
Sylvain Carisey <sylvain@carisey.dev>
2025-12-20 16:50:18 +0100

    feat:classic pull based deploy

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,6 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
 {
-  autoDeploy = import ./autoDeploy.nix;
   bootanimation = import ./bootanimation.nix;
   docker = import ./docker.nix;
   i3Xfce = import ./i3.nix;
