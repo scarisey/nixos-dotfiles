61a224eeab26325fe34b1a95f940798e3bc2f15f
Sylvain Carisey <sylvain@carisey.dev>
2026-01-20 13:50:04 +0100

    chore:update vault

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,7 +10,7 @@
     // {
       copilotCli = inputs.copilot-cli.packages.${final.system}.default;
       snowflakeCli = inputs.snowflake-cli.packages.${final.system}.default;
-      mistral-vibe = inputs.mistral-vibe.packages.${final.system}.default;
+      mistralVibe = inputs.mistral-vibe.packages.${final.system}.default;
       darktable = unstable.darktable;
       immich-unstable = unstable.immich;
       ghostty = inputs.ghostty.packages.${final.system}.default;
