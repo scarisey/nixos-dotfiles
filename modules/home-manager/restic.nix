428174fc9c0147a2372a17a6bdb60dddb5724d97
Sylvain Carisey <sylvain@carisey.dev>
2026-01-20 21:51:24 +0100

    feat(restic):add backup cmd

--- modules/home-manager/restic.nix
+++ modules/home-manager/restic.nix
@@ -6,6 +6,11 @@
 }:
 with lib; let
   cfg = config.scarisey.restic;
+  resticHdd1 = pkgs.writeShellScriptBin "restic-hdd-1" ''
+    export RESTIC_REPOSITORY="/backup/hdd1"
+    export RESTIC_PASSWORD_FILE="${config.sops.secrets."restic/hdd-backup-1/repositoryPwd".path}"
+    ${pkgs.restic}/bin/restic "$@"
+  '';
   resticProton = pkgs.writeShellScriptBin "restic-proton" ''
     export RESTIC_REPOSITORY="/home/sylvain/backupsRestic/"
     export RESTIC_PASSWORD_FILE="${config.sops.secrets."restic/protonDrive".path}"
@@ -22,16 +27,24 @@ with lib; let
 in {
   options.scarisey.restic = {
     enable = mkEnableOption "Restic settings.";
+    all = mkEnableOption "All commands";
+    hddBackup1 = mkEnableOption "Command for hdd backup 1";
   };
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       restic
-      resticCronos
-      resticProton
+    ]
+    ++ optionals cfg.hddBackup1 [
+        resticHdd1
+    ]
+    ++ optionals cfg.all [
+        resticCronos
+        resticProton
     ];
 
-    sops.secrets."restic/protonDrive" = {};
-    sops.secrets."restic/cronos-backups/repositoryPwd" = {};
-    sops.secrets."restic/cronos-backups/backblaze/envFile" = {};
+    sops.secrets."restic/hdd-backup-1/repositoryPwd" = mkIf (cfg.all || cfg.hddBackup1) {};
+    sops.secrets."restic/protonDrive" = mkIf cfg.all {};
+    sops.secrets."restic/cronos-backups/repositoryPwd" = mkIf cfg.all {};
+    sops.secrets."restic/cronos-backups/backblaze/envFile" =  mkIf cfg.all {};
   };
 }
