79cdbe62dbfaff9f59c9b86563bfba7d001ea259
Sylvain Carisey <sylvain@carisey.dev>
2026-02-11 17:33:10 +0100

    feat(nvim):ansi color code

---
+++ modules/home-manager/nvim/lua/plugins/ansi.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    '0xferrous/ansi.nvim',
+    config = function()
+      require('ansi').setup({
+        auto_enable = true,  -- Auto-enable for configured filetypes
+        filetypes = { 'log', 'ansi' },  -- Filetypes to auto-enable
+      })
+    end
+  },
+}
