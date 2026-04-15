86e9bfb499b54f2d2ef4675a603ca72a8c7ce329
Sylvain Carisey <sylvain@carisey.dev>
2026-04-15 20:14:49 +0200

    feat(hyperion):pullix with hm, wip

--- home-manager/sylvain/x86_64-linux/hyperion/home.nix
+++ home-manager/sylvain/x86_64-linux/hyperion/home.nix
@@ -10,6 +10,7 @@
 
   scarisey.myshell.enable = true;
   scarisey.devtools.enable = true;
+  scarisey.devtools.rust = true;
   scarisey.restic = {
     enable = true;
     all = false;
@@ -24,23 +25,23 @@
       type = "GitHub";
       repo = "scarisey/nixos-dotfiles";
       prodSpec = {
-        ref = "hyperion/prod";
+        ref = "feat/hmPullix";
       };
       testSpec = {
-        ref = "hyperion/test";
+        ref = "feat/hmPullix";
       };
     };
     environmentFile = config.sops.secrets."hyperion/nix_config_pullix".path;
-    verbose_logs = false;
+    verbose_logs = true;
     otelHttpEndpoint = "http://localhost:4318/v1/metrics";
-    homeManager = {
-        package = inputs.home-manager.packages.x86_64-linux.home-manager;
-        username = "${config.home.username}";
-    };
   };
 
   sops.secrets."hyperion/wake-titan" = {
     path = "${config.home.homeDirectory}/.local/bin/wake-titan";
     mode = "0550";
+  };
+  sops.secrets."hyperion/nix_config_pullix" = {
+    path = "${config.home.homeDirectory}/.config/pullix/nix_config_pullix";
+    mode = "0400";
   };
 }
