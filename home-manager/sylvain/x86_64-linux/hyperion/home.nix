e755b01f59078fa4351a5dc4f73c16df00dd4bae
Sylvain Carisey <sylvain@carisey.dev>
2026-04-15 12:08:56 +0200

    feat(hyperion):pullix for home manager

--- home-manager/sylvain/x86_64-linux/hyperion/home.nix
+++ home-manager/sylvain/x86_64-linux/hyperion/home.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   config,
+  inputs,
   ...
 }: {
   imports = [
@@ -15,8 +16,37 @@
     hddBackup1 = true;
   };
 
-  sops.secrets."hyperion/wake-titan" = {
-    path = "${config.home.homeDirectory}/.local/bin/wake-titan";
-    mode = "0550";
+  services.pullix = {
+    enable = true;
+    hostname = "hyperion";
+    pollIntervalSecs = 60;
+    flakeRepo = {
+      type = "GitHub";
+      repo = "scarisey/nixos-dotfiles";
+      prodSpec = {
+        ref = "hyperion/prod";
+      };
+      testSpec = {
+        ref = "hyperion/test";
+      };
+    };
+    environmentFile = config.sops.secrets."hyperion/nix_config_pullix".path;
+    verbose_logs = true;
+    otelHttpEndpoint = "http://localhost:4318/v1/metrics";
+    homeManager = {
+      package = inputs.home-manager.packages.x86_64-linux.home-manager;
+      username = "${config.home.username}";
+    };
+  };
+
+  sops.secrets = {
+    "hyperion/nix_config_pullix" = {
+      path = "${config.home.homeDirectory}/.config/pullix/nix_config_pullix";
+      mode = "0400";
+    };
+    "hyperion/wake-titan" = {
+      path = "${config.home.homeDirectory}/.local/bin/wake-titan";
+      mode = "0550";
+    };
   };
 }
