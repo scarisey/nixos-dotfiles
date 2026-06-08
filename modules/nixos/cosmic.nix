ccfd600fe27cc3b501c80d95a9310ae40322985b
Sylvain Carisey <sylvain@carisey.dev>
2026-06-08 17:11:24 +0200

    feat(titan): cosmic DE

---
+++ modules/nixos/cosmic.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  ...
+}:with lib;
+let
+ cfg = config.scarisey.cosmic;
+in {
+  options.scarisey.cosmic = {
+    enable = mkEnableOption "Enable Cosmic Desktop environment.";
+  };
+  config =
+    mkIf cfg.enable
+    {
+      services.xserver.xkb.layout = config.console.keyMap;
+      services.displayManager = {
+        cosmic-greeter.enable = true;
+      };
+      services.desktopManager = {
+        cosmic.enable = true;
+      };
+      programs.dconf.enable = true;
+    };
+}
